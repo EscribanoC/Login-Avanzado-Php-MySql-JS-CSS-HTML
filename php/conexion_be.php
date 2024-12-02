96b800cf073656ed261a8badd24cb4d84ea1989d
EscribanoC <escribano.flores.carlos@iescamas.es>
2024-12-02 17:47:21 +0100

    Archivo de configuración de acceso a la BBDD

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "mycarlos05", "DgL6ftS4", "simulacrocamas");
+$conexion = mysqli_connect("localhost", "mycarlos05", "DgL6ftS4", "simulacrocamas2");
 
 /*/
 if ($conexion){
